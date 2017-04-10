b4f1873ff7a960d531e84b005ffec37419ffd219
David Fokkema <davidfokkema@gmail.com>
2017-04-10 21:58:03 +0200

    Slide met verschillende fouten

--- scripts/slide-fitten-van-lijn.py
+++ scripts/slide-fitten-van-lijn.py
@@ -3,8 +3,8 @@ import numpy as np
 A = 1
 B = .4
 
-def make_dataset(suffix, std=None, show_residuals=False):
-    np.random.seed(1)
+def make_dataset(suffix, seed=1, std=None, show_residuals=False, show_errors=False, larger_errors=True):
+    np.random.seed(seed)
     x = np.linspace(.5, 5.5, 7)
     y = A + B * x
     Y = y.copy()
@@ -17,6 +17,12 @@ def make_dataset(suffix, std=None, show_residuals=False):
             print u, v, w
             if show_residuals:
                 f.write(r"\draw[blue] (%f, %f) -- (%f, %f);" % (u, v, u, w) + '\n')
+            if show_errors:
+                if larger_errors:
+                    error = .05 + .1 * u
+                else:
+                    error = .05 + .1 * (6 - u)
+                f.write(r"\draw (%f, %f) -- (%f, %f);" % (u, v + error, u, v - error) + '\n')
             f.write(r"\fill (%f, %f) circle (1pt);" % (u, v) + '\n')
     print
 
@@ -25,3 +31,5 @@ if __name__ == '__main__':
     make_dataset('1')
     make_dataset('2', std=.3)
     make_dataset('3', std=.3, show_residuals=True)
+    make_dataset('4', seed=4, std=.3, show_errors=True)
+    make_dataset('5', seed=4, std=.3, show_errors=True, larger_errors=False)
